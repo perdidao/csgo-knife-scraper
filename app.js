a3078191f3f1b4adf146c29dfab4e5194331c2df
Lucas Almeida <lucasecalmeida@gmail.com>
2021-03-04 02:30:15 -0300

    Gravando skins no banco

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
-import Express from 'express'
-import Env from 'dotenv'
-import got from 'got'
-import cheerio from 'cheerio'
-import AppSettings from './app/settings.js'
-Env.config()
+import Express from "express";
+import Env from "dotenv";
+import got from "got";
+import cheerio from "cheerio";
+import mysql from "mysql";
+import AppSettings from "./app/settings.js";
+Env.config();
 
-const App = Express()
-AppSettings(App)
+const App = Express();
+AppSettings(App);
 
-App.get('/', (rew, res) => {
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_SECRET,
+  database: process.env.DB_NAME,
+});
+
+connection.connect();
+
+App.get("/", (rew, res) => {
   async function scrapeGutPrices() {
-    const html = await got('https://www.proskins.gg/gut')
-    const $ = cheerio.load(html.body)
-    let data = []
+    const html = await got("https://www.proskins.gg/gut");
+    const $ = cheerio.load(html.body);
+    let data = [];
 
-    $('.info-produto').each(function (i, el) {
-      data.push({
-        title: $(el).find('.nome-produto').text(),
-        price: {
-          default: $(el).find('.preco-venda').text(),
-          promotional: $(el).find('.preco-promocional').text()
+    $(".info-produto").each(function (i, el) {
+      const item = {
+        title: $(el).find(".nome-produto").text(),
+        price: $(el).find(".preco-venda").text(),
+        promotional: $(el).find(".preco-promocional").text(),
+        storeId: 3,
+      };
+      data.push(item);
+      connection.query(
+        "INSERT INTO knifes SET ?",
+        item,
+        function (error, results, fields) {
+          if (error) throw error;
         }
-      })
-    })
+      );
+    });
 
-    return data
+    return data;
   }
 
   scrapeGutPrices()
     .then((data) => {
-      res.render('index', { title: 'Scraper para skins', data })
+      res.render("index", { title: "Scraper para skins", data });
     })
     .catch((e) => {
-      console.log(e)
-    })
-})
+      console.log(e);
+    });
+});
